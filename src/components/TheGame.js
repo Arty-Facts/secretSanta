fc23580a2da6ba5c615404ac89a030c454bab07f
Arturas Aleksandrauskas <artal938@student.liu.se>
2018-12-08 22:56:12 +0100

    added images

--- src/components/TheGame.js
+++ src/components/TheGame.js
@@ -5,7 +5,7 @@ import * as actions from '../actions/authActions';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
-import TextField from 'material-ui/TextField';
+import Avatar from 'material-ui/Avatar';
 
 class TheGame extends Component {
     componentWillReceiveProps(nextProps) {
@@ -24,6 +24,17 @@ class TheGame extends Component {
                             showMenuIconButton={false}
                             title="Secret Santa"
                         />
+                        <div>
+                        <Avatar alt="Remy Sharp" src="https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/31081711_2130308393652502_7549969284168941568_n.jpg?_nc_cat=104&_nc_ht=scontent-arn2-1.xx&oh=0685caf5c315009ee3d65043758ea519&oe=5C9AD7C4" style={styles.bigAvatar}/>
+                        <Avatar alt="Remy Sharp" src="https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/31081711_2130308393652502_7549969284168941568_n.jpg?_nc_cat=104&_nc_ht=scontent-arn2-1.xx&oh=0685caf5c315009ee3d65043758ea519&oe=5C9AD7C4" style={styles.bigAvatar}/>
+                        </div>
+                        <RaisedButton
+                                id="logInButton"
+                                style={styles.button}
+                                //onClick={() => this.props.validateUser(username, password)}
+                            >
+                                Du är secret santa till...
+                            </RaisedButton>
                     </div>
                 </MuiThemeProvider>
             </div>
@@ -38,10 +49,20 @@ const styles = {
     },
     LogInContainer: {
         textAlign: 'center',
+        //backgroundImage: "url('https://www.google.se/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwitkb3Pm5HfAhWKCiwKHdJYBSIQjRx6BAgBEAU&url=https%3A%2F%2Fopenclipart.org%2Fdetail%2F188775%2Fanimated-christmas-tree&psig=AOvVaw1nEyYfoG1DjMKXRB_z5MhZ&ust=1544392199798288')"
     },
     button: {
-        backgroundColor: '#009900',
+        //color: '#009900',
+        width: 200
     },
+    avatar: {
+        margin: 10,
+      },
+    bigAvatar: {
+        margin: '5%',
+        width: '30%',
+        height: 'auto',
+      },
 };
 
 const mapStateToProps = ({ authReducer }) => {
